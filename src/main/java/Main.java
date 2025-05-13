d78a746c99bd073e919cac56f6af683941da6227
silvarge <sep.non2@gmail.com>
2025-05-13 11:15:31 +0900

    Hint: 요세푸스 문제#1158 / 백준
    큐에 대한 이해도가 부족해서 어려움이 있었다.
    큐를 어떻게 사용해야 하는지 정리해야 할 필요가 있다.
    
    - 자료구조, 큐

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
 import brute_force.CandyGame3085;
 import brute_force.NandM15650;
+import data_structure.JosephusProblem1158;
 import java.io.*;
 import java.util.*;
 import math.GetPrimeNumber1929;
 import math.LaundryCeoDonghyuk2720;
 import math.MathIsOnline19532;
 import math.SumOfDivisor17425;
+import math.Warning3029;
 import set_and_map.NumberCard10815;
 import set_and_map.PocketMaster1620;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    NandM15650 sol = new NandM15650();
+    JosephusProblem1158 sol = new JosephusProblem1158();
     sol.solution();
   }
 }
---
+++ src/main/java/data_structure/JosephusProblem1158.java
@@ -0,0 +1,33 @@
+package data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JosephusProblem1158 {
+  public void solution() throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] inp = br.readLine().split(" ");
+    int n = Integer.parseInt(inp[0]);
+    int k = Integer.parseInt(inp[1]);
+
+    StringBuilder sb = new StringBuilder();
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 1; i <= n; i++) {
+      queue.add(i);
+    }
+
+    sb.append("<");
+    while (queue.size() != 1) {
+      for (int i = 0; i < k - 1; i++) {
+        queue.offer(queue.poll());
+      }
+      sb.append(queue.poll() + ", ");
+    }
+    sb.append(queue.poll() + ">");
+
+    System.out.println(sb);
+  }
+}
